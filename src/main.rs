55e55e18bc05f4f4bc0a785c116c35b73651e58f
snakagomi <tanakagoro0319@yahoo.co.jp>
2023-07-15 22:58:46 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::io::{BufRead, BufReader};
+use std::{env, fs::File};
+
+fn main() {
+    let dic_file = "./ejdic-hand-txt/ejdict-hand-utf8.txt";
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("[Usage] english_dictionary word");
+        return;
+    }
+
+    let word = &args[1];
+
+    let fp = File::open(dic_file).unwrap();
+    let reader = BufReader::new(fp);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.find(word) == None {
+            continue;
+        }
+        println!("{}", line);
+    }
+}
